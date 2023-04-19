6cf617e86c117ad132dfb193cbdf3831d1687102
RAJKUMAR SHAH <113999215+Rajkumar-62@users.noreply.github.com>
2023-04-19 19:30:48 +0530

    Delete storage.js

--- storage.js
+++
@@ -1,9 +0,0 @@
-localStorage.setItem('User',JSON.stringify({name:'Ujjawal'}))
-localStorage.setItem('Number',100)
-localStorage.setItem("City","Delhi")
-let keys = Object.keys(localStorage);
-for(let key of keys) 
-{
-    if(key=="User"|| key=="Number"|| key=="City")
-    console.log(`${key}: ${localStorage.getItem(key)}`);
-}
